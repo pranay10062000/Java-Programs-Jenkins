7f3e9db20b86e57c0b5aa36f2fc8403583027016
pranay10062000 <chaitanydumbhare21@gmail.com>
2023-09-12 16:28:39 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Java-Programs-Jenkins
+ Jenkins Tutorial
---
+++ proj1.java
@@ -0,0 +1,9 @@
+/**
+ * proj1
+ */
+public class proj1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+}
